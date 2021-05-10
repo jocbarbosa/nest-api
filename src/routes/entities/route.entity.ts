a0696cfb993f7cf98cc78715584141bc40671c12
Joao C Barbosa <joaao@outlook.com>
2021-05-09 23:17:01 -0300

    feat: map route entity

--- src/routes/entities/route.entity.ts
+++ src/routes/entities/route.entity.ts
@@ -1,1 +1,28 @@
-export class Route { }
+import { Prop, Schema, raw, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from 'mongoose';
+
+export type RouteDocument = Route & Document;
+
+@Schema()
+export class Route {
+  @Prop()
+  title: string;
+
+  @Prop(
+    raw({
+      latitude: { type: Number },
+      longitude: { type: Number }
+    })
+  )
+  startPosition: { latitude: number; longitude: number };
+
+  @Prop(
+    raw({
+      latitude: { type: Number },
+      longitude: { type: Number }
+    })
+  )
+  endPosition: { latitude: number; longitude: number };
+}
+
+export const RouteSchema = SchemaFactory.createForClass(Route);
