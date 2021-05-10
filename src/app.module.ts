27f57ea9cfd1e9df2c59893485cd97f14feeddc8
Joao C Barbosa <joaao@outlook.com>
2021-05-09 23:00:58 -0300

    feat: add routes nestjs resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoutesModule } from './routes/routes.module';
 
 @Module({
-  imports: [],
+  imports: [
+    RoutesModule,
+    MongooseModule.forRoot('mongodb://root:root@db/nest?authSource=admin', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
